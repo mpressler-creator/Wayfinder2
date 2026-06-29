f50b7c785966d6e2193f3eddf7f7641a37f39235
mpressler-creator <mpressler@hotmail.com>
2026-06-28 20:30:39 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins: [react()] });
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  root: '.',              // ensure Vite looks in the repo root
+  publicDir: 'public',    // ensure Vite uses your public folder
+  plugins: [react()],
+  build: {
+    outDir: 'dist'
+  }
+})
+
